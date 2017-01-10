310e6722d8c0a48f7394dc18b79db2b59bd8c5a6
Ammar <Ammar>
2017-01-09 21:08:20 -0500

    challenge 1 code push

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,22 @@
+var name = " Ferg";
+var greeting = " Hello";
+console.log(greeting + name)
+
+var x = 1125;
+var answer = x % 2;
+console.log (answer)
+
+var a = true;
+var b = false;
+var c = true;
+
+console.log(a||b||c)
+console.log(a&&b&&c)
+
+var answer1= 10-3*5/4+202%3
+var answer2=(3*(5/4)+(202%3))
+console.log(answer1)
+console.log(answer2)
+
+
+
